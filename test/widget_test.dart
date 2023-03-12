7c19452ba0a68bed5b97aafec7a027b2877070b0
JasonTan02 <103154789+JasonTan02@users.noreply.github.com>
2023-03-12 11:52:32 +0800

    event planner added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world/Testing/main.dart';
 
-import 'package:hello_world/main.dart';
+//import '../lib/Testing/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
